c7091a4036f0720bc4d1cab9a15c397e1add9f1c
Steve Ahlers <ahlers.steve@gmail.com>
2012-01-16 16:43:08 -0500

    First commit

---
+++ src/com/js/mobile/LeaderBoard.java
@@ -0,0 +1,49 @@
+package com.js.mobile;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: steveahlers
+ * Date: 1/16/12
+ * Time: 4:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LeaderBoard extends ListActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, PLAYERS));
+
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    static final String[] PLAYERS = new String[] {
+            "Superman", "JGyselinck", "SAhlers", "CodeMonkey", "GSDExpert",
+            "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
+            "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
+            "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
+            "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
+            "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
+            "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory",
+            "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
+            "Cote d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde",
+            "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
+            "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo"
+    };
+
+}
---
+++ src/com/js/mobile/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.js.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
