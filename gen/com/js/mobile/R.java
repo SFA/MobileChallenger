ed869733a38b29baf493e29bf0e6a3d91580fadd
John Gyselinck <johngyselinck@gmail.com>
2012-01-17 20:51:35 -0500

    Added the login layout and the login functionality.

--- gen/com/js/mobile/R.java
+++ gen/com/js/mobile/R.java
@@ -10,9 +10,18 @@ package com.js.mobile;
 public final class R {
     public static final class attr {
     }
+    public static final class id {
+        public static final int login_button=0x7f040005;
+        public static final int password_text=0x7f040003;
+        public static final int txt_password=0x7f040004;
+        public static final int txt_username=0x7f040002;
+        public static final int username_text=0x7f040001;
+        public static final int welcome_text=0x7f040000;
+    }
     public static final class layout {
         public static final int list_item=0x7f020000;
-        public static final int main=0x7f020001;
+        public static final int login=0x7f020001;
+        public static final int main=0x7f020002;
     }
     public static final class string {
         public static final int app_name=0x7f030000;
--- src/com/js/mobile/LoginActivity.java
+++ src/com/js/mobile/LoginActivity.java
@@ -1,15 +1,73 @@
 package com.js.mobile;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity
 {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    public void onCreate(Bundle icicle){
+        super.onCreate(icicle);
+
+        //load up the layout
+        setContentView(R.layout.login);
+
+        //get the button resource in the xml file and assign it to a local variable of type Button
+        Button launch = (Button)findViewById(R.id.login_button);
+
+        //this is the action listener
+        launch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //This gets the resources in the xml file and assigns it to a local variable of type EditText
+                EditText usernameEditText = (EditText) findViewById(R.id.txt_username);
+                EditText passwordEditText = (EditText) findViewById(R.id.txt_password);
+
+                //The getText() gets the current value of the text box
+                //The toString() converts the value to String data type then assigns it to a variable of type String
+                String sUserName = usernameEditText.getText().toString();
+                String sPassword = passwordEditText.getText().toString();
+
+                //this just catches the error if the program can't locate the GUI stuff
+                if (usernameEditText == null || passwordEditText == null) {
+                    PopUp("Crap!", "Couldn't find the 'txt_username' or 'txt_password' EditView in main.xml");
+                } else {
+                    //Display the username and the password in string format
+                    PopUp("Logging in", "Username: " + sUserName + "\nPassword: " + sPassword);
+                    Intent myIntent = new Intent(getBaseContext(), LeaderBoardActivity.class);
+                    myIntent.putExtra("userName", sUserName);
+                    startActivity(myIntent);
+//                    setContentView(R.layout.main);
+                }
+            }
+        }); //end of launch.setOnclickListener
+    }
+
+    public void PopUp(String title, String message){
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //do stuff onclick of YES
+//                        finish();
+                    }
+                })
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //do stuff onclick of CANCEL
+                        Toast.makeText(getBaseContext(), "You clicked CANCEL", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
     }
 }
