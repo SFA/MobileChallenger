deb013ab00be8c75ba53b49acfe59d08478464cc
John Gyselinck <johngyselinck@gmail.com>
2012-01-20 10:43:14 -0500

    Added the login layout and the login functionality.

--- services/emailService.php
+++ services/emailService.php
@@ -1,5 +1,24 @@
 <?php
-    $email = $_POST['email'];
+    require_once "Mail.php";
 
-    $message =
+    $email_from = "johngyselinck@gmail.com";
+    $email_to = "john.gyselinck@sparcedge.com";
+    $email_subject = "Test Email";
+    $email_message = "Test Email Message.";
+
+    $headers = array('From' => $email_from, 'To'=>$email_to, 'Subject'=>$email_subject);
+
+    $host = "localhost";
+    $port = "25";
+    $username = "john";
+    $password = "pass";
+
+    $smtp = Mail::factory('smtp', array('host'=>$host, 'port'=>$port, 'auth'=>false, 'username'=>$username, 'password'=>$password));
+    $mail = $smtp->send($email_to, $headers, $email_message);
+
+    if (PEAR::isError($mail)) {
+        echo("<p>" . $mail->getMessage() . "</p>");
+    } else {
+        echo("<p>Message successfully sent!</p>");
+    }
 ?>
