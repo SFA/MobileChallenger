17cca2aa1258ee58690d680d4dfcf89226c9b65e
John Gyselinck <johngyselinck@gmail.com>
2012-01-20 18:18:42 -0500

    Test Data....yea!!!

--- dbscripts/insertTestUsers.sql
+++ dbscripts/insertTestUsers.sql
@@ -1,4 +1,42 @@
 select * from mobilechallengeuser;
 
-insert into mobilechallengeuser values ('jgyselinck', 'pass','johngyselinck@gmail.com', 'John', 'Gyselinck');
+select * from challenge;
+
+select * from matches;
+
+insert into mobilechallengeuser values ('jgyselinck', 'pass','john.gyselinck@sparcedge.com', 'John', 'Gyselinck');
 insert into mobilechallengeuser values ('sahlers', 'sahlers', 'steve.ahlers@sparcedge.com', 'Steve', 'Ahlers');
+insert into mobilechallengeuser values ('jblow', 'pass', 'john.gyselinck@sparcedge.com', 'Steve', 'Ahlers');
+
+insert into challenge (challengee, challenger) values ('sahlers', 'jgyselinck');
+insert into challenge (challengee, challenger) values ('jgyselinck', 'sahlers');
+insert into challenge (challengee, challenger) values ('jblow', 'jgyselinck');
+insert into challenge (challengee, challenger) values ('sahlers', 'jblow');
+insert into challenge (challengee, challenger) values ('jgyselinck', 'jblow');
+insert into challenge (challengee, challenger) values ('jblow', 'sahlers');
+
+insert into matches (challenge_id) values (1);
+insert into matches (challenge_id) values (2);
+insert into matches (challenge_id) values (3);
+insert into matches (challenge_id) values (4);
+insert into matches (challenge_id) values (5);
+insert into matches (challenge_id) values (6);
+
+insert into game (match_id) values (1);
+insert into game (match_id) values (2);
+insert into game (match_id) values (3);
+insert into game (match_id) values (4);
+insert into game (match_id) values (5);
+insert into game (match_id) values (6);
+
+insert into outcome(game_id, challenger_score, challengee_score) values (1, 21, 19);
+insert into outcome(game_id, challenger_score, challengee_score) values (2, 10, 21);
+insert into outcome(game_id, challenger_score, challengee_score) values (3, 8, 21);
+insert into outcome(game_id, challenger_score, challengee_score) values (4, 21, 12);
+insert into outcome(game_id, challenger_score, challengee_score) values (5, 5, 21);
+insert into outcome(game_id, challenger_score, challengee_score) values (6, 21, 17);
+
+
+
+
+
--- dbscripts/queriesForStats.sql
+++ dbscripts/queriesForStats.sql
@@ -1,4 +1,20 @@
-select * 
+select m.username,
+       m.email,
+       m.first_name,
+       m.last_name,
+       c.challengee,
+       o.challenger_score,
+       o.challengee_score,
+       case when(o.challenger_score > o.challengee_score)
+        then "W"
+        else "L"
+       end "Result"
     from mobilechallengeuser m, 
-         challenge c 
-     where m.username = c.challenger;-- or m.username = m.challengee);
+         challenge c,
+         matches ma,
+         game g,
+         outcome o
+     where m.username = c.challenger
+            and c.id = ma.challenge_id
+            and ma.id = g.match_id
+            and g.id = o.game_id;
